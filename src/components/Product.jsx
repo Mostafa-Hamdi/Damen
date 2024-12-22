157bbb711ad1f9f8888d6a91ff286b2b2162e069
Mostafa Hamdi <149847073+Mostafa-Hamdi@users.noreply.github.com>
2024-12-22 11:53:06 +0200

    Update Product.jsx

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -32,6 +32,19 @@ class Product extends Component {
         <h2 className="price">300 EGP</h2>
         <div className="add-cart">
           <button onClick={this.handleAddToCart}>Add to Cart</button>
+          <button className="love">
+            <svg viewBox="0 0 256 256" xmlns="http://www.w3.org/2000/svg">
+              <rect fill="none" height="256" width="256" />
+              <path
+                d="M128,216S28,160,28,92A52,52,0,0,1,128,72h0A52,52,0,0,1,228,92C228,160,128,216,128,216Z"
+                fill="none"
+                stroke="#000"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="8"
+              />
+            </svg>
+          </button>
         </div>
       </div>
     );
